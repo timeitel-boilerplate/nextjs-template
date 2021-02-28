3b2e6bf7dfa2da4495651d7e65af43dd9ba17c05
Tim Eitel <timeitel@outlook.com>
2021-02-28 08:37:29 +0800

    wip: theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,16 @@
+export const theme = {
+  palette: {
+    primary: {
+      main: '#556cd6'
+    },
+    secondary: {
+      main: '#19857b'
+    },
+    error: {
+      main: '#ef5350'
+    },
+    background: {
+      default: '#fff'
+    }
+  }
+};
