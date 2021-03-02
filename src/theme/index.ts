dbeac97c9c7d7f3c21de8b2602345bad9a37bb39
Tim Eitel <timeitel@outlook.com>
2021-03-02 21:16:58 +0800

    wip: theming

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,2 @@
 export * from './theme';
-export * from './colors';
 export * from './GlobalStyles';
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,5 +1,16 @@
-import { colors } from './colors';
 import {} from 'styled-components';
+
+const colors = {
+  primary: '#2563eb',
+  secondary: '#9ca3af',
+  white: '#ffffff',
+  info: '#059669',
+  grey100: '#f3f4f6',
+  grey300: '#d1d5db',
+  grey500: '#6b7280',
+  grey700: '#374151',
+  black: '#111827'
+};
 
 export const theme = {
   colors,
