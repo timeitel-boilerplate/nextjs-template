a5cc11e4001da57ed76663ab2d9240e7ab431227
Tim Eitel <tim@komo.digital>
2021-03-10 07:55:34 +0800

    wip: styles

--- src/theme/GlobalStyles.tsx
+++ src/theme/GlobalStyles.tsx
@@ -6,14 +6,20 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0; 
     margin: 0; 
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif; 
+    height: 100%;
   }
 
   body {
     padding: 0; 
     margin: 0; 
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;  
-    color: ${theme.colors.grey700};
-    background: ${theme.colors.white};
+    color: ${theme.color.grey700};
+    background: ${theme.color.white};
+    height: 100%;
+  }
+
+  #__next {
+    height: 100%;
   }
 
   a { 
